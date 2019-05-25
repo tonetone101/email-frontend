c485c4deb7a68fdd7244ccee22b88809034aa810
Antkeo1 <sleav143@gmail.com>
2019-05-25 19:51:58 -0400

    route flow is fixed when loggin in and out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>
+
+      default:
+        return <li><a href='/api/logout'>Logout</a></li>
+    }
+  }
 
   render() {
     return (
-      <div>
         <nav>
           <div className="nav-wrapper">
-            <a class="left brand-logo">Email</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a>Login With Google</a></li>
+            <a className="left brand-logo">
+              Email
+            </a>
+            <ul className="right">
+              {this.renderContent()}
             </ul>
           </div>
         </nav>
-      </div>
     )
   }
 }
 
-export default Header
+const mapStateToProps = ({auth}) => {
+  return {auth}
+}
+
+export default connect(mapStateToProps)(Header)
