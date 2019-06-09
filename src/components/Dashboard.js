caed155189f0f33eb94e8cdd10c7c4a83af90ffd
Antkeo1 <sleav143@gmail.com>
2019-06-09 17:52:36 -0400

    created a create new survey button for dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <div>DashBoard</div>
-  )
-}
+    <div>
+      DashBoard
+      <div className='fixed-action-btn'>
+        <Link to='/surveys/new' className='btn-floating btn-large red'>
+          <i className='material-icons'>add</i>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard
