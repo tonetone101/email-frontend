d923bf70d43f98f4912720b37c552e5d4fd03479
Antkeo1 <sleav143@gmail.com>
2019-06-15 16:27:31 -0400

    created a new action creator for webhooks

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,6 +11,7 @@ export const handleToken = (token) => async dispatch => {
     dispatch({type: FETCH_USER, payload: res.data })
   };
 
+// to send out surveys
 export const submitSurvey = (values, history) => async dispatch => {
     const res = await axios.post('/api/surveys', values)
     history.push('/surveys')
